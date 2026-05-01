15306ad3b23abfbdbbd09f724ab383f390629eca
andreia-hemobras <andreia.hemobras@gmail.com>
2026-05-01 17:55:54 -0300

    GET products paged

--- src/main/java/dev/andreia/dscommerce/services/ProductService.java
+++ src/main/java/dev/andreia/dscommerce/services/ProductService.java
@@ -4,7 +4,10 @@ import dev.andreia.dscommerce.dto.ProductDTO;
 import dev.andreia.dscommerce.entities.Product;
 import dev.andreia.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -12,8 +15,15 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> page = repository.findAll(pageable);
+        return page.map(product -> new ProductDTO(product));
     }
 }
