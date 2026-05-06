1e5070aca0949f026525987ed659e4f946a13264
andreia-hemobras <andreia.hemobras@gmail.com>
2026-05-06 10:46:06 -0300

    POST new product

--- src/main/java/dev/andreia/dscommerce/services/ProductService.java
+++ src/main/java/dev/andreia/dscommerce/services/ProductService.java
@@ -5,6 +5,7 @@ import dev.andreia.dscommerce.entities.Product;
 import dev.andreia.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -12,8 +13,21 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+        Product product = new Product();
+        product.setName(dto.getName());
+        product.setDescription(dto.getDescription());
+        product.setPrice(dto.getPrice());
+        product.setImgUrl(dto.getImgUrl());
+
+        repository.save(product);
         return new ProductDTO(product);
     }
 }
