a5419f2f59d29ce7f8d2221f1ebfce45945f8540
andreia-hemobras <andreia.hemobras@gmail.com>
2026-05-11 10:59:37 -0300

    Bean Validation PT. 2

--- src/main/java/dev/andreia/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/dev/andreia/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package dev.andreia.dscommerce.controllers.handlers;
 
 import dev.andreia.dscommerce.dto.CustomError;
+import dev.andreia.dscommerce.dto.ValidationError;
 import dev.andreia.dscommerce.services.exceptions.DatabaseException;
 import dev.andreia.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,5 +27,14 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException ex, HttpServletRequest request){
         CustomError err = new CustomError(Instant.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request){
+        ValidationError error = new ValidationError(Instant.now(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Dados inválidos", request.getRequestURI());
+        for(FieldError f : ex.getBindingResult().getFieldErrors()){
+            error.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
     }
 }
---
+++ src/main/java/dev/andreia/dscommerce/dto/ValidationError.java
@@ -0,0 +1,22 @@
+package dev.andreia.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Instant moment, Integer status, String error, String path) {
+        super(moment, status, error, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String field, String message){
+        errors.add(new FieldMessage(field, message));
+    }
+}
