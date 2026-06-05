1a79db0c679bf2d9107422bcffc9106de304b7b6
andreia-hemobras <andreia.hemobras@gmail.com>
2026-06-05 15:16:43 -0300

    ImgUrl on OrderItemDto

--- src/main/java/dev/andreia/dscommerce/dto/OrderItemDto.java
+++ src/main/java/dev/andreia/dscommerce/dto/OrderItemDto.java
@@ -8,15 +8,18 @@ public class OrderItemDto {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
+
 
     public OrderItemDto() {
     }
 
-    public OrderItemDto(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDto(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDto(OrderItem entity) {
@@ -24,6 +27,7 @@ public class OrderItemDto {
         name = entity.getProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
+        imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -44,5 +48,9 @@ public class OrderItemDto {
 
     public Double getSubTotal(){
         return price * quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 }
